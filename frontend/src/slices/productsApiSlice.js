8587410ce2d88d8c5cb3dc1236e1122bd8a2b12b
Sallahtech <bubacarrsallah@gmail.com>
2023-09-30 00:47:14 +0300

    added basic redux store structure using redux-toolkit for fetching data from the backend

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,17 @@
+import { PRODUCTS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => PRODUCTS_URL,
+    }),
+    getProductDetails: builder.query({
+      query: (id) => `${PRODUCTS_URL}/${id}`,
+    }),
+  }),
+  keepUnusedDataFor: 5,
+});
+
+export const { useGetProductsQuery, useGetProductDetailsQuery } =
+  productsApiSlice;
